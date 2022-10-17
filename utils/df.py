5d29ab1e89a094a8a3d7883304de3ccd4a67d5e4
valderpan <haoranpan@foxmail.com>
2022-10-17 10:44:12 +0800

    Happy 26th Birthday! Debug utils.df

--- utils/df.py
+++ utils/df.py
@@ -22,12 +22,19 @@ def read2List(file):
         _vL = [l for l in (line.strip() for line in f)]
     return _vL
 
-def subdf(vL,df,col_name):
-    if col_name not in [col for col in df]:
-        log.error('{} is not in the column name of dataframe ! Please check if the input ab is correct !'.format(col_name))
-        sys.exit()
-    else:
-        sdf = df[df[col_name].isin(vL)]
+def subdf(vL,df,col_name,col_type):
+    if col_type == 'name':
+        if col_name not in [col for col in df]:
+            log.error('{} is not in the column name of dataframe ! Please check if the input ab is correct !'.format(col_name))
+            sys.exit()
+        else:
+            sdf = df[df[col_name].isin(vL)]
+    elif col_type == 'index':
+        if int(col_name) > len(df.columns.values.tolist()):
+            log.error('Index {} is not in the column name of dataframe ! Please check if the input ab is correct !'.format(col_name))
+            sys.exit()
+        else:
+            sdf = df[df.iloc[:,int(col_name)].isin(vL)]
     return sdf
 
 
@@ -47,11 +54,14 @@ def extractSubdf(args):
     pReq.add_argument('valuefile', 
             help='Input file with values stored by `\\n` partition')
     pReq.add_argument('dataframe', 
-            help='Input the dataframe file to be extracted')
-    pReq.add_argument('-c','--colname', type=str, required=True,
+            help='Input the dataframe file to be extracted\nNote: This file must have a header !!!')
+    pReq.add_argument('-c','--colname', required=True,
             help='Specify which column of the dataframe file to extract based on')
+    pReq.add_argument('-t','--coltype', required=True,choices=['name','index'],
+            help='Specify column type of the dataframe file to extract based on, \
+                it can be a column name or a column index\nNote:Use only if the -c parameter is present')
     pReq.add_argument('-o', '--output', required=True,
-            help='Specify the name of the output file')
+            help='The name of the output file')
     pOpt.add_argument('-h', '--help', action='help',
             help='show help message and exit.')
     
@@ -61,9 +71,9 @@ def extractSubdf(args):
     vl = read2List(args.valuefile)
     check_file_exists(args.dataframe)
     df = read_file(args.dataframe)
-    res = subdf(vl,df,args.colname)
+    res = subdf(vl,df,args.colname,args.coltype)
     log.info('The results file is output to {}'.format(args.output))
-    res.to_excel(args.output,header=True,index=False)
+    res.to_csv(args.output,header=False,index=False,sep='\t')
 
 
 def main():
